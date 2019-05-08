1f5b0029db043959a22c1c9ea957655971d987d9
Unknown <elian.hamon@viacesi.fr>
2019-05-08 15:12:11 +0200

    Disassembler now prints Program counter and address registers in hex

--- src/GUI/DisassemblerFrame.cpp
+++ src/GUI/DisassemblerFrame.cpp
@@ -23,9 +23,9 @@ DisassemblerFrame::DisassemblerFrame(SCC68070& core, MainFrame* parent, const wx
     for(uint8_t i = 0; i < 8; i++)
     {
         d[i] = new wxTextCtrl(registersPanel, wxID_ANY, "D" + std::to_string(i) + ": " + std::to_string(cpu.D[i]), wxPoint(0, i * 23), wxSize(125, 23), wxTE_READONLY); d[i]->SetBackgroundColour(*wxWHITE);
-        a[i] = new wxTextCtrl(registersPanel, wxID_ANY, "A" + std::to_string(i) + ": " + std::to_string(cpu.A[i]), wxPoint(0, i * 23 + 189), wxSize(125, 23), wxTE_READONLY); a[i]->SetBackgroundColour(*wxWHITE);
+        a[i] = new wxTextCtrl(registersPanel, wxID_ANY, "A" + std::to_string(i) + ": " + toHex(cpu.A[i]),    wxPoint(0, i * 23 + 189), wxSize(125, 23), wxTE_READONLY); a[i]->SetBackgroundColour(*wxWHITE);
     }
-    pc = new wxTextCtrl(registersPanel, IDDisassemblerpc, "PC: " + std::to_string(cpu.PC), wxPoint(0, 377), wxSize(125, 23), wxTE_READONLY); pc->SetBackgroundColour(*wxWHITE);
+    pc = new wxTextCtrl(registersPanel, IDDisassemblerpc, "PC: " + toHex(cpu.PC), wxPoint(0, 377), wxSize(125, 23), wxTE_READONLY); pc->SetBackgroundColour(*wxWHITE);
     sr = new wxTextCtrl(registersPanel, IDDisassemblersr, "SR: " + toBinString(cpu.SR, 16), wxPoint(0, 400), wxSize(125,23), wxTE_READONLY); sr->SetBackgroundColour(*wxWHITE);
 
     wxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
@@ -66,10 +66,12 @@ void DisassemblerFrame::PaintEvent()
         for(uint8_t i = 0; i < 8; i++)
         {
             d[i]->SetLabelText("D" + std::to_string(i) + ": " + std::to_string(cpu.D[i]));
-            a[i]->SetLabelText("A" + std::to_string(i) + ": " + std::to_string(cpu.A[i]));
+            a[i]->SetLabelText("A" + std::to_string(i) + ": " + toHex(cpu.A[i]));
         }
-        pc->SetLabelText("PC: " + std::to_string(cpu.PC));
+        pc->SetLabelText("PC: " + toHex(cpu.PC));
         sr->SetLabelText("SR: " + toBinString(cpu.SR, 16));
+        if(currentRow > cpu.instructionsBuffer.size())
+            currentRow = cpu.instructionsBuffer.size()-1;
         for(; currentRow < cpu.instructionsBuffer.size(); currentRow++)
             disassembler->AppendText(cpu.instructionsBuffer[currentRow] + "\n");
     }
