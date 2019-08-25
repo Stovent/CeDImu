a8867eca4a179669d14ea0381c34a6b800a1adf5
Unknown <elian.hamon@viacesi.fr>
2019-08-25 23:06:01 +0200

    MC212 logs only with DEBUG defined

--- src/Cores/MCD212/DRAMInterface.cpp
+++ src/Cores/MCD212/DRAMInterface.cpp
@@ -5,17 +5,23 @@ uint8_t MCD212::GetByte(const uint32_t& addr)
 {
     if(addr >= 0x4FFFE0 && addr <= 0x4FFFFF)
     {
-        out << std::hex << app->cpu->PC << "\tGet register: " << addr << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tGet register: 0x" << addr << std::endl;
+#endif // DEBUG
         return registers[addr-0x4FFFE0] & 0x00FF;
     }
     else if(addr <= 0x4FFFDF)
     {
-        out << std::hex << app->cpu->PC << "\tGet byte \t\t at address " << addr << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tGet byte \t\t at address 0x" << addr << std::endl;
+#endif // DEBUG
         return memory[addr];
     }
     else
     {
-        out << std::hex << app->cpu->PC << "\tGet byte out of range: " << addr << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tGet byte out of range: 0x" << addr << std::endl;
+#endif // DEBUG
         return 0;
     }
 }
@@ -29,18 +35,24 @@ uint16_t MCD212::GetWord(const uint32_t& addr)
     }
     if(addr >= 0x4FFFE0 && addr <= 0x4FFFFF)
     {
-        out << std::hex << app->cpu->PC << "\tGet WORD register: " << addr << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tGet WORD register: 0x" << addr << std::endl;
+#endif // DEBUG
         return registers[addr-0x4FFFE0];
     }
     else if(addr < 0x4FFFDF)
     {
+#ifdef DEBUG
         if(addr != app->cpu->PC)
-            out << std::hex << app->cpu->PC << "\tGet word \t\t at address " << addr << std::endl;
+            out << std::hex << app->cpu->PC << "\tGet word \t\t at address 0x" << addr << std::endl;
+#endif // DEBUG
         return memory[addr] << 8 | memory[addr + 1];
     }
     else
     {
-        out << std::hex << app->cpu->PC << "\tGet word out of range: " << addr << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tGet word out of range: 0x" << addr << std::endl;
+#endif // DEBUG
         return 0;
     }
 }
@@ -54,17 +66,23 @@ uint32_t MCD212::GetLong(const uint32_t& addr)
     }
     if(addr >= 0x4FFFE0 && addr <= 0x4FFFFF)
     {
-        out << std::hex << app->cpu->PC << "\tGet LONG register: " << addr << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tGet LONG register: 0x" << addr << std::endl;
+#endif // DEBUG
         return registers[addr-0x4FFFE0] << 16 | registers[addr-0x4FFFDF];
     }
     else if(addr < 0x4FFFDF)
     {
-        out << std::hex << app->cpu->PC << "\tGet long \t\t at address " << addr << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tGet long \t\t at address 0x" << addr << std::endl;
+#endif // DEBUG
         return memory[addr] << 24 | memory[addr + 1] << 16 | memory[addr + 2] << 8 | memory[addr + 3];
     }
     else
     {
-        out << std::hex << app->cpu->PC << "\tGet long out of range: " << addr << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tGet long out of range: 0x" << addr << std::endl;
+#endif // DEBUG
         return 0;
     }
 }
@@ -73,16 +91,22 @@ void MCD212::SetByte(const uint32_t& addr, const uint8_t& data)
 {
     if(addr >= 0x4FFFE0 && addr <= 0x4FFFFF)
     {
-        out << std::hex << app->cpu->PC << "\tSet BYTE register: " << addr << " value " << data << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tSet BYTE register: 0x" << addr << " value " << data << std::endl;
+#endif // DEBUG
     }
     else if(addr <= 0x4FFFDF)
     {
-        out << std::hex << app->cpu->PC << "\tSet byte " << std::to_string(data) << " \tat address " << addr << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tSet byte " << std::to_string(data) << " \tat address 0x" << addr << std::endl;
+#endif // DEBUG
         memory[addr] = data;
     }
     else
     {
-        out << std::hex << app->cpu->PC << "\tSet byte out of range: " << addr << " value " << std::to_string(data) << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tSet byte out of range: 0x" << addr << " value " << std::to_string(data) << std::endl;
+#endif // DEBUG
     }
 }
 
@@ -90,17 +114,23 @@ void MCD212::SetWord(const uint32_t& addr, const uint16_t& data)
 {
     if(addr >= 0x4FFFE0 && addr <= 0x4FFFFF)
     {
-        out << std::hex << app->cpu->PC << "\tSet register: " << addr << " value " << data << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tSet register: 0x" << addr << " value " << data << std::endl;
+#endif // DEBUG
     }
     else if(addr < 0x4FFFDF)
     {
-        out << std::hex << app->cpu->PC << "\tSet word " << std::to_string(data) << " \tat address " << addr << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tSet word " << std::to_string(data) << " \tat address 0x" << addr << std::endl;
+#endif // DEBUG
         memory[addr] = data >> 8;
         memory[addr+1] = data;
     }
     else
     {
-        out << std::hex << app->cpu->PC << "\tSet word out of range: " << addr << " value " << std::to_string(data) << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tSet word out of range: 0x" << addr << " value " << std::to_string(data) << std::endl;
+#endif // DEBUG
     }
 }
 
@@ -108,11 +138,15 @@ void MCD212::SetLong(const uint32_t& addr, const uint32_t& data)
 {
     if(addr >= 0x4FFFE0 && addr <= 0x4FFFFF)
     {
-        out << std::hex << app->cpu->PC << "\tSet LONG register: " << addr << " value " << data << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tSet LONG register: 0x" << addr << " value " << data << std::endl;
+#endif // DEBUG
     }
     else if(addr < 0x4FFFDF)
     {
-        out << std::hex << app->cpu->PC << "\tSet long " << std::to_string(data) << " \tat address " << addr << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tSet long " << std::to_string(data) << " \tat address 0x" << addr << std::endl;
+#endif // DEBUG
         memory[addr]   = data >> 24;
         memory[addr+1] = (data & 0x00FF0000) >> 16;
         memory[addr+2] = (data & 0x0000FF00) >> 8;
@@ -120,6 +154,8 @@ void MCD212::SetLong(const uint32_t& addr, const uint32_t& data)
     }
     else
     {
-        out << std::hex << app->cpu->PC << "\tSet long out of range: " << addr << " value " << std::to_string(data) << std::endl;
+#ifdef DEBUG
+        out << std::hex << app->cpu->PC << "\tSet long out of range: 0x" << addr << " value " << std::to_string(data) << std::endl;
+#endif // DEBUG
     }
 }
