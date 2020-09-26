ff41d774af8ecdd4aaa3117599aae0e909de9186
Default Profile <elian.hamon@viacesi.fr>
2020-09-26 12:46:32 +0200

    remove slave addressing modes

--- src/cores/MC68HC705C8/MemoryAccess.cpp
+++ src/cores/MC68HC705C8/MemoryAccess.cpp
@@ -1,49 +1,5 @@
 #include "MC68HC705C8.hpp"
 
-uint8_t MC68HC705C8::GetByteIndexed()
-{
-    return GetByte(X);
-}
-
-void MC68HC705C8::SetByteIndexed(const uint8_t value)
-{
-    SetByte(X, value);
-}
-
-uint8_t MC68HC705C8::GetByteIndexed8()
-{
-    return GetByte(GetNextByte() + X);
-}
-
-//void MC68HC705C8::SetByteIndexed8(const uint8_t value)
-//{
-//    SetByte(GetNextByte() + X, value);
-//}
-
-uint8_t MC68HC705C8::GetByteIndexed16()
-{
-    uint16_t addr = GetNextByte() << 8;
-    addr |= GetNextByte();
-    return GetByte(addr + X);
-}
-
-//void MC68HC705C8::SetByteIndexed16(const uint8_t value)
-//{
-//    uint16_t addr = GetNextByte() << 8;
-//    addr |= GetNextByte();
-//    SetByte(addr + X, value);
-//}
-
-//uint8_t MC68HC705C8::GetByteRelative()
-//{
-//
-//}
-//
-//void MC68HC705C8::SetByteRelative(const uint8_t value)
-//{
-//
-//}
-
 uint8_t MC68HC705C8::GetByte(const uint16_t addr)
 {
     if(addr < 0x2000)
