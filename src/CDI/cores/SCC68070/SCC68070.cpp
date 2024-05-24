86d61e500afa4e2c3daa321f3d79bbee786138b7
Stovent <elian.hamon@viacesi.fr>
2024-05-25 01:55:14 +0200

    Forgot this

--- src/CDI/cores/SCC68070/SCC68070.cpp
+++ src/CDI/cores/SCC68070/SCC68070.cpp
@@ -53,12 +53,11 @@ void SCC68070::Reset()
 }
 
 /** \brief Requests the CPU to process the given exception.
- * \param vector The vector number of the exception.
- * \param data An additional data stored in the Exception.
+ * \param ex The exception to process.
  */
-void SCC68070::PushException(const ExceptionVector vector, const uint16_t data)
+void SCC68070::PushException(const Exception& ex)
 {
-    exceptions.emplace(vector, data);
+    exceptions.emplace(ex);
 }
 
 /** \brief Trigger interrupt with LIR1 level.
