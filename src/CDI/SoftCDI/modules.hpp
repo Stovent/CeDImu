707ebc97c6b134fd01235c915a7834e48e49bdf9
Stovent <elian.hamon@viacesi.fr>
2025-06-17 16:07:42 -0400

    minor build tools changes

--- src/CDI/SoftCDI/modules.hpp
+++ src/CDI/SoftCDI/modules.hpp
@@ -4,18 +4,18 @@
 #include <cstdint>
 #include <span>
 
-constexpr uint8_t CIAPDRIV_DATA[] = {
+inline constexpr uint8_t CIAPDRIV_DATA[] = {
 #embed "build/CIAPDRIV"
 };
-constexpr std::span<const uint8_t> CIAPDRIV{CIAPDRIV_DATA};
+inline constexpr std::span<const uint8_t> CIAPDRIV{CIAPDRIV_DATA};
 
 // #include "../../SoftCDI/include/CSD_450.h"
 // #include "../../SoftCDI/include/NVDRV.h"
 // #include "../../SoftCDI/include/VIDEO.h"
 
-constexpr uint8_t SYSGO_DATA[] = {
+inline constexpr uint8_t SYSGO_DATA[] = {
 #embed "build/SYSGO"
 };
-constexpr std::span<const uint8_t> SYSGO{SYSGO_DATA};
+inline constexpr std::span<const uint8_t> SYSGO{SYSGO_DATA};
 
 #endif // CDI_SOFTCDI_MODULES_HPP
