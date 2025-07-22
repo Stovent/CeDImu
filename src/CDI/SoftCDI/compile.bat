7060e7b5c280a79cac1a357dea8798516f7a197f
Stovent <elian.hamon@viacesi.fr>
2025-07-21 23:31:08 -0400

    Link against CDISYS.L

--- src/CDI/SoftCDI/compile.bat
+++ src/CDI/SoftCDI/compile.bat
@@ -5,5 +5,5 @@ set GRPUSER=0.0
 echo %1
 r68 %1.a -o=build/%1.r
 if ERRORLEVEL 1 pause
-l68 build/%1.r -n=%1 -o=build/%1 -l=c:/LIB/SYS.L
+l68 build/%1.r -n=%1 -o=build/%1 -l=c:/LIB/SYS.L -l=c:/LIB/CDISYS.L
 if ERRORLEVEL 1 pause
