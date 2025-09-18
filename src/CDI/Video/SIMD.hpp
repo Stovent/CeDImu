18f1b8e88e58720734b102abba701dbc69dbfb14
Stovent <elian.hamon@viacesi.fr>
2025-09-17 23:07:51 -0400

    Prepare for the possibility of <simd> being implemented

--- src/CDI/Video/SIMD.hpp
+++ src/CDI/Video/SIMD.hpp
@@ -7,23 +7,35 @@
 
 #include "Pixel.hpp"
 
-#include <version>
-
-#include <experimental/simd>
-namespace stdx = std::experimental;
-#if __cpp_lib_simd
-#warning "SIMD is no longer experimental"
+#if __has_include(<simd>)
+// #   include <simd>
+#   warning not yet implemented with <simd> header, using <experimental/simd> instead
 #endif
+// #elif __has_include(<experimental/simd>)
+#   include <experimental/simd>
+// #else
+// #    error Missing <simd> or <experimental/simd> headers
+// #endif
 
 namespace Video
 {
 
+#if __has_include(<simd>)
+#   warning not yet implemented with <simd> header, using <experimental/simd> instead
+#endif
+// #elif __has_include(<experimental/simd>)
+
+namespace stdx = std::experimental;
 using SIMDNativePixel = stdx::native_simd<Pixel::ARGB32>;
 using SIMDNativePixelSigned = stdx::native_simd<std::make_signed_t<Pixel::ARGB32>>;
 template<size_t WIDTH>
 using SIMDFixedPixelSigned = stdx::fixed_size_simd<std::make_signed_t<Pixel::ARGB32>, WIDTH>;
 using SIMDNativeU8 = stdx::native_simd<uint8_t>;
 using SIMDFixedS16 = stdx::rebind_simd_t<int16_t, SIMDNativeU8>;
+
+// #else
+// #   error Missing <simd> or <experimental/simd> headers
+// #endif
 
 inline constexpr size_t SIMD_SIZE = SIMDNativePixel::size();
 
